889cafc9b5c6f13cb6a6d764e705b7671f478c4a
David Forer <david.j.forer@gmail.com>
2017-10-30 23:18:11 +1100

    added listings to users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,9 @@
 class Listing < ApplicationRecord
     has_attached_file :image, styles: { medium: "150x", thumb: "100x100>" }, default_url: "missing.jpeg"
     validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+
+    validates :name, :price, :description, presence: true
+    validates :price, numericality: { greater_than: 0 }
+
+    belongs_to :user
 end
