9bdf8e221a1327b8d559f316f9e61e9a76c48a7e
David Forer <david.j.forer@gmail.com>
2017-11-01 19:08:26 +1100

    added lots of pics

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ApplicationRecord
-    has_attached_file :image, styles: { medium: "150x", thumb: "100x100>" }, default_url: "missing.jpeg"
+    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "missing.jpeg"
     validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
 
     validates :name, :price, :description, presence: true
